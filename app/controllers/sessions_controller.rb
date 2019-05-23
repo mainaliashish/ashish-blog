9964867777284b7a834a8796d45de59b78f4b650
ashishmainalee <ashishmainalee@gmail.com>
2019-05-23 20:34:04 +0545

    make it better

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		# debugger
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id
+			flash[:success] = "You have successfully logged in."
+			redirect_to users_path(user)
+		else
+			flash.now[:danger] = "Error : There was something wrong with your login information."
+			render 'new'
+
+	end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "You have logged out Successfully."
+		redirect_to root_path
+	end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,58 @@
+class UsersController < ApplicationController
+
+	before_action :set_user, only: [:edit, :update, :show, :destroy]
+	before_action :require_user, only: [:edit, :update, :destroy]
+	before_action :require_same_user, only: [:edit, :update]
+
+	def index
+	@users = User.paginate(page: params[:page], per_page: 5)
+	end
+
+	def new
+	@user = User.new
+	render 'users/new'
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:info] = "Welcome to Weblog #{@user.username}"
+			redirect_to articles_path
+		else
+			render :action => 'new'
+			#redirect_to new_user_path(@user)
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+		flash[:info] = "User Was Updated Successfully!"
+		redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+		@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+	end
+
+	private
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
+	end
+
+	def require_same_user
+		if current_user != @user
+			flash[:danger] = "You can only edit your own account."
+			redirect_to root_path
+		end
+	end
+end
