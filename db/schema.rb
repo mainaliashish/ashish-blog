c38d9720bf2d45a8d91b24b7338c9203770e08aa
ashishmainalee <ashishmainalee@gmail.com>
2019-05-24 16:27:37 +0545

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_041710) do
+ActiveRecord::Schema.define(version: 2019_05_24_101802) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +23,11 @@ ActiveRecord::Schema.define(version: 2019_05_24_041710) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "articles_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
