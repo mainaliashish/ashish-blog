2d573db2e98d45119b77a2dfa79f210501c3df49
ashishmainalee <ashishmainalee@gmail.com>
2019-05-25 11:17:55 +0545

    Category Articles complete fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:info] = "Welcome to Weblog #{@user.username}"
-			redirect_to articles_path
+			redirect_to user_path(@user)
 		else
 			render :action => 'new'
 			#redirect_to new_user_path(@user)
